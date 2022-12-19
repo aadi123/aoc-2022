283adab1d1bd4b8bca100f6159b036fbf2ee7970
Aaditya Murthy <aadi.murthyananth@gmail.com>
2022-12-19 17:30:10 -0600

    Update

--- day1/Main.hs
+++ day1/Main.hs
@@ -1,4 +1,9 @@
+module Main where
+
+main :: IO ()
 main = do
+    raw <- readFile "input.text"
     let currentCalorieSum = 0
     let maxCalories = 0
     input <- getLine
+    let nums = map read $ lines raw :: [Int]
