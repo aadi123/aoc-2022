f19da71bd2ec16ecc0475b41eec4bd122bfd07d0
Aaditya Murthy <aadi.murthyananth@gmail.com>
2022-12-19 17:53:16 -0600

    Update

--- day1/Main.hs
+++ day1/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 main :: IO ()
 main = do
-    raw <- readFile "input.txt"
     let currentCalorieSum = 0
     let maxCalories = 0
+    raw <- readFile "input.txt"
     putStrLn raw
