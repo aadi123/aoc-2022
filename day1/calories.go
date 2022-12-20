44a4a61bb267d4b520a2580b1259b172a0ebcb53
Aaditya Murthy <aadi.murthyananth@gmail.com>
2022-12-20 15:46:00 -0600

    Update

--- day1/calories.go
+++ day1/calories.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"container/heap"
 	"fmt"
 	"os"
 	"strconv"
@@ -13,10 +14,65 @@ func check(e error) {
 	}
 }
 
-func main() {
+// print the elf with the most calories
+// func part1() {
+// 	// declare individual elf calories and max Calories
+// 	elfCals := 0
+// 	maxCals := 0
+// 	cals := 0
+// 	// read input
+// 	f, err := os.Open("./input.txt")
+// 	check(err)
+// 	defer f.Close()
+
+// 	scanner := bufio.NewScanner(f)
+
+// 	// while line is not new line, scan line and add to elfCals
+// 	for scanner.Scan() {
+// 		for scanner.Text() != "" {
+// 			cals, _ = strconv.Atoi(scanner.Text())
+// 			elfCals += cals
+// 			scanned := scanner.Scan()
+// 			if !scanned && scanner.Err() == nil {
+// 				break
+// 			}
+// 		}
+
+// 		if elfCals > maxCals {
+// 			maxCals = elfCals
+// 		}
+
+// 		elfCals = 0
+// 	}
+
+// 	fmt.Println(maxCals)
+
+// }
+
+// An IntHeap is a min-heap of ints.
+type MaxHeap []int
+
+func (h MaxHeap) Len() int           { return len(h) }
+func (h MaxHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h MaxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *MaxHeap) Push(x any) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.(int))
+}
+
+func (h *MaxHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func part2() {
 	// declare individual elf calories and max Calories
 	elfCals := 0
-	maxCals := 0
 	cals := 0
 
 	// read input
@@ -25,6 +81,9 @@ func main() {
 	defer f.Close()
 
 	scanner := bufio.NewScanner(f)
+
+	h := &MaxHeap{}
+	heap.Init(h)
 
 	// while line is not new line, scan line and add to elfCals
 	for scanner.Scan() {
@@ -37,12 +96,20 @@ func main() {
 			}
 		}
 
-		if elfCals > maxCals {
-			maxCals = elfCals
-		}
+		heap.Push(h, elfCals)
 
 		elfCals = 0
 	}
 
-	fmt.Println(maxCals)
+	topThreeTotalCals := 0
+	for i := 0; i < 3; i++ {
+		topThreeTotalCals += heap.Pop(h).(int)
+	}
+
+	fmt.Println(topThreeTotalCals)
+}
+
+func main() {
+	// part1()
+	part2()
 }
